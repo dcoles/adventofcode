cee16f9a8559817462fe2eadb4f0bb9d63b418a8
David Coles <coles.david@gmail.com>
2019-01-02 15:15:19 -0800

    day18: Part 2
    Represent map as single `Vec<char>` and add memoization via `HashMap` to
    detect cycles and use them to skip over the majority of iterations.

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,28 +1,29 @@
+use std::collections::HashMap;
 use std::fs;
 
 fn main() {
     // Part 1
     run(10, true);
+
+    // Part 2
+    run(1000000000 , false);
 }
 
 fn run(n: usize, print: bool) {
     let mut input = read_input("input.txt");
     if print { input.print() };
 
-    for _n in 0..n {
-        input.tick();
-        if print { input.print() };
-    }
+    input.run_until(n);
+
+    if print { input.print() };
 
     let mut n_wooded = 0;
     let mut n_lumberyards = 0;
-    for row in &input.cells {
-        for &tile in row {
-            if tile == '|' {
-                n_wooded += 1;
-            } else if tile == '#' {
-                n_lumberyards += 1;
-            }
+    for &tile in &input.cells {
+        if tile == '|' {
+            n_wooded += 1;
+        } else if tile == '#' {
+            n_lumberyards += 1;
         }
     }
 
@@ -37,72 +38,93 @@ fn read_input(filename: &str) -> Map {
 
     let mut cells = Vec::new();
     let mut width = std::usize::MAX;
+    let mut height = 0;
     for line in input.lines() {
         let line: Vec<char> = line.chars().collect();
         width = width.min(line.len());
-        cells.push(line);
+        height += 1;
+        cells.extend(line);
     }
 
-    Map { cells, width }
+    Map { cells, width, height, next_state: HashMap::new(), minutes: 0 }
 }
 
 struct Map {
-    cells: Vec<Vec<char>>,
+    cells: Vec<char>,
     width: usize,
+    height: usize,
+    next_state: HashMap<Vec<char>, (usize, Vec<char>)>,
+    minutes: usize,
 }
 
 type Pos = (usize, usize);
 
 impl Map {
     fn print(&self) {
-        for row in &self.cells {
-            for &tile in row {
-                match tile {
-                    '#' => print!("\x1b[31m{}\x1b[0m", tile),  // Red
-                    '|' => print!("\x1b[32m{}\x1b[0m", tile),  // Green
-                    _ => print!("{}", tile),
-                }
+        for (n, &tile) in self.cells.iter().enumerate() {
+            match tile {
+                '#' => print!("\x1b[31m{}\x1b[0m", tile),  // Red
+                '|' => print!("\x1b[32m{}\x1b[0m", tile),  // Green
+                _ => print!("{}", tile),
             }
-            println!();
+            if n % self.width == self.width - 1 {
+                println!();
+            }
         }
         println!();
     }
 
     fn get(&self, pos: Pos) -> char {
-        self.cells[pos.1][pos.0]
+        let n = self.width * pos.1 + pos.0;
+
+        self.cells[n]
+    }
+
+    fn run_until(&mut self, minutes: usize) {
+        while self.minutes < minutes {
+            // Look for a cycle
+            if let Some((n, next)) = self.next_state.get(&self.cells) {
+                let delta = self.minutes - *n;
+                self.minutes += ((minutes - self.minutes) / delta) * delta;
+            }
+            self.tick();
+        }
     }
 
     fn tick(&mut self) {
         let mut new_cells = self.cells.clone();
 
-        for y in 0..self.cells.len() {
+        for y in 0..self.height {
             for x in 0..self.width {
                 let pos = (x, y);
                 let tile = self.get(pos);
                 let adj = self.adjacent(pos);
+                let n = self.width * y + x;
                 match tile {
                     '.' => if self.count_tiles_at(&adj, '|') >= 3 {
-                        new_cells[pos.1][pos.0] = '|';
+                        new_cells[n] = '|';
                     },
                     '|' => if self.count_tiles_at(&adj, '#') >= 3 {
-                        new_cells[pos.1][pos.0] = '#';
+                        new_cells[n] = '#';
                     },
                     '#' => if self.count_tiles_at(&adj, '#') == 0 || self.count_tiles_at(&adj, '|') == 0 {
-                        new_cells[pos.1][pos.0] = '.';
+                        new_cells[n] = '.';
                     },
                     t => panic!("Unknown tile {:?}", t),
                 }
             }
         }
 
+        self.next_state.insert(self.cells.clone(), (self.minutes, new_cells.clone()));
         self.cells = new_cells;
+        self.minutes += 1;
     }
 
     fn adjacent(&self, pos: Pos) -> Vec<Pos> {
         let mut result = Vec::new();
         for (xoff, yoff) in &[(0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)] {
             let (x, y) = (pos.0 as i32 + xoff, pos.1 as i32 + yoff);
-            if 0 <= x && x < self.width as i32 && 0 <= y && y < self.cells.len() as i32 {
+            if 0 <= x && x < self.width as i32 && 0 <= y && y < self.height as i32 {
                 result.push((x as usize, y as usize));
             }
         }
