b96f81f41ec4c4c178fd69f9f52b453a529aa37f
David Coles <coles.david@gmail.com>
2019-12-01 22:21:01 -0800

    2019: Day 2 (part 2)

--- 2019/day02/src/main.rs
+++ 2019/day02/src/main.rs
@@ -16,6 +16,23 @@ fn main() {
     program[2] = 2;
     run(&mut program);
     println!("Part 1: Position 0 = {}", program[0]);
+
+    // Part 2
+    let target = 19690720;
+    'outer: for noun in 0..=99usize {
+        for verb in 0..=99usize {
+            let mut program = input.clone();
+            program[1] = noun;
+            program[2] = verb;
+            run(&mut program);
+
+            if program[0] == target {
+                let answer = 100 * noun + verb;
+                println!("Part 2: Inputs {}, {} give {} (answer: {})", noun, verb, target, answer);
+                break 'outer;
+            }
+        }
+    }
 }
 
 fn read_input<T: AsRef<Path>>(path: T) -> Vec<usize> {
@@ -24,8 +41,8 @@ fn read_input<T: AsRef<Path>>(path: T) -> Vec<usize> {
 }
 
 fn run(program: &mut Vec<usize>) {
-    for i in (0..program.len()).step_by(4) {
-        let (op, x1, x2, x3) = (program[i], program[i+1], program[i+2], program[i+3]);
+    for ip in (0..program.len()).step_by(4) {
+        let (op, x1, x2, x3) = (program[ip], program[ip+1], program[ip+2], program[ip+3]);
         match op {
             OP_ADD => program[x3] = program[x1] + program[x2],
             OP_MUL => program[x3] = program[x1] * program[x2],
