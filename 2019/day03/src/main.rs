8679e99ee9ade77f561291068260053c229a17ed
David Coles <coles.david@gmail.com>
2019-12-03 15:31:45 -0800

    2019: Day 3 Cleanup and refactor
    This is mostly extracting the walking into a dedicated function and
    adding some helper types for Pos and Direction to make it easier to work
    with these values.

--- 2019/day03/src/main.rs
+++ 2019/day03/src/main.rs
@@ -1,126 +1,186 @@
 use std::path::Path;
-use std::fs;
+use std::{fs, ops, fmt};
 use std::collections::HashSet;
 
-const ORIGIN: Pos = (0, 0);
+const ORIGIN: Pos = Pos::new(0, 0);
 
 fn main() {
     let input = read_input("input.txt");
     for wire in &input {
-        println!("{}", wire.iter().map(Move::to_string).collect::<Vec<_>>().join(","));
+        println!("{}", wire.iter().map(|m| m.to_string()).collect::<Vec<_>>().join(","));
     }
 
     // Part 1
     let mut grid: HashSet<Pos> = HashSet::new();
     let mut intersections: Vec<Pos> = Vec::new();
     for wire in &input {
+        // Walk the wire's path
+        let path = walk(&wire);
+
         // Check for any intersections
-        let mut positions: Vec<Pos> = Vec::new();
-        let mut pos = ORIGIN;
-        for m in wire {
-            let line_positions = m.new_pos(pos);
-            for &pos in &line_positions {
-                if grid.contains(&pos) {
-                    intersections.push(pos);
-                };
-                positions.push(pos);
+        for &pos in &path {
+            if grid.contains(&pos) {
+                intersections.push(pos);
             }
-            pos = *line_positions.last().unwrap();
         }
 
-        // Add all the positions
-        for pos in positions {
+        // Add the wire to to grid
+        for &pos in &path {
             grid.insert(pos);
         }
     }
 
-    intersections.sort_by_key(|&p| distance(ORIGIN, p));
-    let closest = intersections[0];
-    println!("Part 1: {:?} (distance: {})", closest, distance(ORIGIN, closest));
+    let closest_intersection = intersections.iter()
+        .map(|&p| (p, p.distance(ORIGIN)))
+        .min_by_key(|&(_, distance)| distance)
+        .expect("Expected non-zero intersections");
+    println!("Part 1: Closest intersection: {} (distance: {})", closest_intersection.0, closest_intersection.1);
 
     // Part 2
-    let mut signal_delays = Vec::new();
+    let mut min_signal_delay = std::u32::MAX;
     for &intersection in &intersections {
         let mut steps = 0;
         for wire in &input {
-            let mut pos = ORIGIN;
-            'outer: for m in wire {
-                let line_positions = m.new_pos(pos);
-                for &pos in &line_positions {
-                    steps += 1;
-                    if pos == intersection {
-                        break 'outer;
-                    }
+            // Walk the wire's path until we hit the intersection
+            for pos in walk(&wire) {
+                steps += 1;
+                if pos == intersection {
+                    break;
                 }
-                pos = *line_positions.last().unwrap();
             }
         }
-        signal_delays.push(steps);
+        min_signal_delay = min_signal_delay.min(steps);
     }
-    signal_delays.sort();
-    println!("Part 2: Fewest combined steps: {}", signal_delays[0]);
+    println!("Part 2: Fewest combined steps: {}", min_signal_delay);
 }
 
+/// Read a list of move-lists from a file.
 fn read_input<T: AsRef<Path>>(path: T) -> Vec<Vec<Move>> {
     let input = fs::read_to_string(path).expect("Failed to read input");
     let mut result = Vec::new();
     for line in input.lines() {
-        let moves: Vec<Move> = line.trim().split(",").map(Move::from_str).collect();
+        let moves: Vec<Move> = line.trim().split(',').filter_map(Move::parse).collect();
         result.push(moves);
     }
 
     result
 }
 
-type Pos = (i32, i32);
+/// Follow a move-list and return the step-by-step path followed.
+fn walk(movelist: &[Move]) -> Vec<Pos> {
+    let mut path = Vec::new();
 
-fn distance(p1: Pos, p2: Pos) -> i32 {
-    (p1.0 - p2.0).abs() + (p1.1 - p2.1).abs()
+    let mut pos = ORIGIN;
+    for m in movelist {
+        let unit = m.direction().unit();
+        for _ in 0..m.distance() {
+            pos += unit;
+            path.push(pos);
+        }
+    }
+
+    path
 }
 
-enum Move {
-    Up(u32),
-    Down(u32),
-    Left(u32),
-    Right(u32),
+#[derive(Hash,Eq,PartialEq,Copy,Clone)]
+struct Pos {
+    x: i32,
+    y: i32,
+}
+
+impl Pos {
+    const fn new(x: i32, y: i32) -> Pos {
+        Pos { x, y }
+    }
+
+    fn distance(self, p: Pos) -> i32 {
+        (self.x - p.x).abs() + (self.y - p.y).abs()
+    }
+}
+
+impl ops::Add<Pos> for Pos {
+    type Output = Pos;
+
+    fn add(self, rhs: Pos) -> Self::Output {
+        Pos { x: self.x + rhs.x, y: self.y + rhs.y }
+    }
+}
+
+impl ops::AddAssign for Pos {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+
+impl fmt::Display for Pos {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}, {}", self.x, self.y)
+    }
+}
+
+#[derive(Eq,PartialEq,Copy,Clone)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn parse(s: &str) -> Option<Direction> {
+        match s {
+            "U" => Some(Direction::Up),
+            "D" => Some(Direction::Down),
+            "L" => Some(Direction::Left),
+            "R" => Some(Direction::Right),
+            _ => None,
+        }
+    }
+
+    fn unit(self) -> Pos {
+        match self {
+            Direction::Up => Pos::new(0, 1),
+            Direction::Down => Pos::new(0, -1),
+            Direction::Left => Pos::new(-1, 0),
+            Direction::Right => Pos::new(1, 0),
+        }
+    }
+
+    fn to_str(self) -> &'static str {
+        match self {
+            Direction::Up => "U",
+            Direction::Down => "D",
+            Direction::Left => "L",
+            Direction::Right => "R",
+        }
+    }
+}
+
+#[derive(Eq,PartialEq,Copy,Clone)]
+struct Move {
+    direction: Direction,
+    distance: u32,
 }
 
 impl Move {
-    fn from_str(string: &str) -> Move {
-        let val = string[1..].parse::<u32>().expect("Failed to parse move");
-        match &string[..1] {
-            "U" => Move::Up(val),
-            "D" => Move::Down(val),
-            "L" => Move::Left(val),
-            "R" => Move::Right(val),
-            c => panic!("Unknown direction {}", c),
-        }
+    fn parse(s: &str) -> Option<Move> {
+        let direction = Direction::parse(&s[..1])?;
+        let distance = s[1..].parse::<u32>().ok()?;
+
+        Some(Move { direction, distance })
     }
 
-    fn to_string(&self) -> String {
-        match self {
-            &Move::Up(v) => format!("U{}", v),
-            &Move::Down(v) => format!("D{}", v),
-            &Move::Left(v) => format!("L{}", v),
-            &Move::Right(v) => format!("R{}", v),
-        }
+    fn direction(self) -> Direction {
+        self.direction
     }
 
-    fn new_pos(&self, start: Pos) -> Vec<Pos> {
-        let mut result = Vec::new();
-        let (dir, dist) = match self {
-            &Move::Up(v) => ((0, 1), v),
-            &Move::Down(v) => ((0, -1), v),
-            &Move::Left(v) => ((-1, 0), v),
-            &Move::Right(v) => ((1, 0), v),
-        };
+    fn distance(self) -> u32 {
+        self.distance
+    }
+}
 
-        let mut pos = start;
-        for _ in 0..dist {
-            pos = (pos.0 + dir.0, pos.1 + dir.1);
-            result.push(pos);
-        }
-
-        result
+impl fmt::Display for Move {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}{}", self.direction.to_str(), self.distance)
     }
 }
