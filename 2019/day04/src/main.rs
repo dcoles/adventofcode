149d033d42b55c88ce8d80e48d23d60e28023963
David Coles <coles.david@gmail.com>
2019-12-03 21:58:29 -0800

    2019: Day 4 (part 2)

--- 2019/day04/src/main.rs
+++ 2019/day04/src/main.rs
@@ -1,48 +1,60 @@
-const INPUT: (u32, u32) = (372037, 905157);
+use std::collections::HashMap;
+
+const INPUT: std::ops::RangeInclusive<u32> = 372037..=905157;
 
 fn main() {
-    assert!(valid(111111));
-    assert!(!valid(223450));
-    assert!(!valid(123789));
+    // Part 1
+    assert!(valid1(111111));
+    assert!(!valid1(223450));
+    assert!(!valid1(123789));
 
-    let mut count = 0;
-    for n in INPUT.0..=INPUT.1 {
-        if valid(n) {
-           count += 1;
-        }
-    }
+    let count = INPUT.filter(|&n| valid1(n)).count();
     println!("Part 1: Number of passwords that meet the criteria: {}", count);
+
+    // Part 2
+    assert!(valid2(112233));
+    assert!(!valid2(123444));
+    assert!(valid2(111122));
+
+    let count = INPUT.filter(|&n| valid2(n)).count();
+    println!("Part 2: Number of passwords that meet the criteria: {}", count);
 }
 
-fn valid(n: u32) -> bool {
-    // The digits never decrease
-    let mut prev_digit = 0;
-    for pos in (0..6).rev() {
-        let digit = digit(n, pos);
-        if digit < prev_digit {
+fn valid1(n: u32) -> bool {
+    digits_never_decrease(n) && count_digits(n).values().any(|&n| n >= 2)
+}
+
+fn valid2(n: u32) -> bool {
+    digits_never_decrease(n) && count_digits(n).values().any(|&n| n == 2)
+}
+
+fn digits_never_decrease(n: u32) -> bool {
+    let mut last_digit = 0;
+    for digit in digits(n) {
+        if digit < last_digit {
             return false;
         }
-        prev_digit = digit;
-    }
-
-    // Two adjacent digits are the same
-    let mut two_adjacent = false;
-    let mut last_digit = digit(n, 0);
-    for pos in 1..6 {
-        let digit = digit(n, pos);
-        if digit == last_digit {
-            two_adjacent = true;
-            break
-        };
         last_digit = digit;
     }
-    if ! two_adjacent {
-        return false;
-    }
 
-    return true;
+    true
 }
 
-fn digit(n: u32, pos: u32) -> u32 {
-    n / 10u32.pow(pos) % 10
+fn count_digits(n: u32) -> HashMap<u32, u32> {
+    let mut counter = HashMap::new();
+    for digit in digits(n) {
+        *counter.entry(digit).or_default() += 1;
+    }
+    counter
+}
+
+fn digits(mut n: u32) -> Vec<u32> {
+    let mut digits = Vec::new();
+    while n > 0 {
+        digits.push(n % 10);
+        n /= 10;
+    }
+
+    digits.reverse();
+    digits
 }
