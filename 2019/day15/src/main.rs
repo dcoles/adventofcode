2c5e656eb925934ee6f9b0a8625bbd234414f673
David Coles <coles.david@gmail.com>
2019-12-15 16:52:50 -0800

    2019: Day 15 (part 2)

--- 2019/day15/src/main.rs
+++ 2019/day15/src/main.rs
@@ -1,8 +1,7 @@
 use intcode::emulator::{IntcodeEmulator, Program, Exception, Word};
 use std::convert::{TryFrom, TryInto};
 use std::collections::{HashMap, HashSet};
-use std::{io, ops, thread, env};
-use std::io::Write;
+use std::{ops, thread, env};
 use std::time::Duration;
 
 const UNKNOWN: char = ' ';
@@ -11,8 +10,8 @@ const OPEN: char = '.';
 const TARGET: char = '*';
 const DROID: char = '@';
 const DEAD: char = '+';
-const DEAD_END: u32 = std::u32::MAX;
-const BEL: &str = "";  // Set to "\x07" if you feel your life lacks excitement
+const OXYGEN: char = 'O';
+const DEAD_END: u32 = std::u32::MAX;  // Cost of tiles leading to a dead-end
 const ORIGIN: Pos = Pos::new(0, 0);
 const WIDTH: u32 = 80;
 const HEIGHT: u32 = 60;
@@ -31,6 +30,7 @@ fn draw(tile: char, pos: Pos) {
         DROID => "\x1B[32m",  // Green
         OPEN => "\x1B[34m",  // Blue
         DEAD => "\x1B[33m",  // Yellow
+        OXYGEN => "\x1B[36m",  // Cyan
         _ => "",
     };
     print!("\x1B[{};{}H{}{}\x1B[m", SCREEN_OFFSET_Y as i32 - pos.y + 1, SCREEN_OFFSET_X as i32 + pos.x + 1, color, tile);
@@ -42,8 +42,10 @@ fn print_status(message: &str) {
 }
 
 fn main() {
+    // The --turbo flag skips all animation delays
     let turbo = env::args().skip(1).any(|arg| &arg == "--turbo");
 
+    // Part 1
     let program = Program::from_file("input.txt").expect("Failed to read input");
 
     let mut droid = Droid::new(&program, ORIGIN);
@@ -53,8 +55,8 @@ fn main() {
     print!("\x1B[?25l");  // Hide cursor
     print!("\x1B[8;{};{}t", HEIGHT, WIDTH);  // Resize console
 
-    // Find the leak
-    let mut leak = None;
+    // Part 1: Find the broken O₂ system
+    let mut o2_system = None;
     while let Some(command) = planner.plan(droid.pos) {
         // Clear the droid from the map
         draw( planner.get_tile(droid.pos), droid.pos);
@@ -70,25 +72,50 @@ fn main() {
                 draw( OPEN, target);
             },
             Status::MovedAndFoundTarget => {
-                leak = Some(target);
+                o2_system = Some(target);
                 planner.update_map(target, TARGET);
                 draw( TARGET, target);
             },
         }
 
         draw( DROID, droid.pos);
-        io::stdout().flush().expect("Failed to flush stdout");
+        print_status!("{:?} (distance: {})", droid.pos, planner.distance_from_origin(droid.pos));
+
+        if !turbo {
+            //thread::sleep(Duration::from_millis(1000 / FPS));
+        }
+    }
+
+    // Found the leak!
+    let o2_system = o2_system.expect("Failed to find the O₂ system");
+    let distance = planner.distance_from_origin(o2_system);
+    draw(planner.get_tile(droid.pos), droid.pos); // Hide the droid, it's work is done
+
+    // Part 2: Fill the map with O₂
+    let mut oxygenated: HashSet<Pos> = [o2_system].iter().copied().collect();
+    let mut edge: HashSet<Pos> = planner.adjacent(o2_system).into_iter().collect();
+    let mut t = 0;
+    while !edge.is_empty() {
+        let mut new_edge = HashSet::new();
+        for pos in edge.drain() {
+            oxygenated.insert(pos);
+            draw(OXYGEN, pos);
+            new_edge.extend(planner.adjacent(pos).into_iter().filter(|&pos| !oxygenated.contains(&pos)));
+        }
+
+        edge = new_edge;
+        t += 1;
+
+        print_status!("t: {} min", t);
 
         if !turbo {
             thread::sleep(Duration::from_millis(1000 / FPS));
         }
     }
 
-    let leak = leak.expect("No leak found");
-    let distance = planner.distance_from_origin(leak);
-    print_status!("Found leak at {:?} (distance: {})", leak, distance);
-
     print!("\x1Bc");  // Reset the terminal
+    println!("Part 1: Found the O₂ system at {:?} (distance: {})", o2_system, distance);
+    println!("Part 2: Time required to fill map with O₂: {} min", t);
 }
 
 struct Planner {
@@ -107,6 +134,7 @@ impl Planner {
         Planner { map, cost, distance, unexplored: HashSet::new() }
     }
 
+    /// Plan the next move
     fn plan(&mut self, current_pos: Pos) -> Option<MovementCommand> {
         use MovementCommand::*;
 
@@ -181,6 +209,16 @@ impl Planner {
     fn is_dead_end(&self, pos: Pos) -> bool {
         self.cost.get(&pos).copied().unwrap_or(0) == DEAD_END
     }
+
+    /// Adjacent tiles to this position
+    fn adjacent(&self, pos: Pos) -> Vec<Pos> {
+        use MovementCommand::*;
+        [North, South, East, West].iter()
+            .copied()
+            .map(|c| pos + c.direction())
+            .filter(|&p| self.get_tile(p) != WALL)
+            .collect()
+    }
 }
 
 struct Droid {
@@ -206,17 +244,13 @@ impl Droid {
                 Exception::Output(word) => {
                     let status: Status = word.try_into().expect("Unknown status");
                     match status {
-                        Status::Wall => {
-                            print_status!("{}* Bonk! *", BEL);
-                        },
                         Status::Moved => {
-                            print_status!("");
                             self.moved(movement);
                         },
                         Status::MovedAndFoundTarget => {
-                            print_status!("");
                             self.moved(movement);
                         },
+                        _ => (),
                     }
                     return status;
                 },
