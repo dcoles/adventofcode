5d5b81e107e2c886ce456242500671fc8236ca1c
David Coles <coles.david@gmail.com>
2019-12-24 12:58:27 -0800

    2019: Day 22 Refactor shuffling into dedicated class

--- 2019/day22/src/main.rs
+++ 2019/day22/src/main.rs
@@ -4,15 +4,14 @@ use std::convert::TryFrom;
 
 type Signed = i128;
 type Unsigned = u128;
-type Vector = [Unsigned; 2];
+type Vector2 = [Unsigned; 2];
 
 const DEAL_NEW_STACK: &str = "deal into new stack";
 const CUT: &str = "cut ";
 const DEAL_WITH_INCREMENT: &str = "deal with increment ";
 const N_CARDS: Unsigned = 10007;
 const M_CARDS: Unsigned = 119315717514047;
-const N_SHUFFLES: Unsigned = 101741582076661;
-const X: Vector = [1, 0];
+const M_SHUFFLES: Unsigned = 101741582076661;
 
 fn main() {
     let techniques = read_input("input.txt");
@@ -20,7 +19,7 @@ fn main() {
     // Part 1
     let pos = 2019;
     for c in 0..M_CARDS {
-        if evaluate(shuffle(&techniques, N_CARDS), c, N_CARDS) == 2019 {
+        if Shuffle::from_techniques(&techniques, N_CARDS).evaluate(c) == 2019 {
             println!("Part 1: Position of card {}: {}", pos, c);
             break;
         }
@@ -28,103 +27,142 @@ fn main() {
 
     // Part 2
     let pos = 2020;
-    let card = evaluate(repeat(shuffle(&techniques, M_CARDS), N_SHUFFLES - 1, M_CARDS), pos, M_CARDS);
-    println!("Part 2: After shuffling {} cards {} times, the card at position {} is: {}", M_CARDS, N_SHUFFLES, pos, card);
+    let card = Shuffle::from_techniques(&techniques, M_CARDS).repeat(M_SHUFFLES - 1).evaluate(pos);
+    println!("Part 2: After shuffling {} cards {} times, the card at position {} is: {}", M_CARDS, M_SHUFFLES, pos, card);
 }
 
-/// Find the card at `pos`, given a `shuffle`
-fn evaluate(shuffle: Vector, pos: Unsigned, n_cards: Unsigned) -> Unsigned {
-    // Formula: ax + b;  where v = [a, b]
-    (shuffle[0] * pos + shuffle[1]) % n_cards
-}
+fn read_input<T: AsRef<Path>>(path: T) -> Vec<Technique> {
+    let contents = fs::read_to_string(path).expect("Failed to read input");
 
-/// Repeat a shuffle `v`, `n` times.
-fn repeat(shuffle: Vector, n: Unsigned, n_cards: Unsigned) -> Vector {
-    // Calculate the powers-of-2 repeats less than `n`
-    let mut powers = vec![shuffle];
-    while 1 << (powers.len() as Unsigned - 1) < n {
-        let v = powers[powers.len() - 1];
-        powers.push(apply(v, v, n_cards));
+    let mut techniques = Vec::new();
+    for line in contents.lines() {
+        let technique = match line {
+            DEAL_NEW_STACK => Technique::DealNewStack,
+            line if line.starts_with(CUT) => {
+                let n = line[CUT.len()..].parse().expect("Failed to parse value");
+
+                Technique::Cut(n)
+            },
+            line if line.starts_with(DEAL_WITH_INCREMENT) => {
+                let n = line[DEAL_WITH_INCREMENT.len()..].parse().expect("Failed to parse value");
+
+                Technique::DealWithIncrement(n)
+            }
+            line => panic!("Unknown line: {}", line),
+        };
+
+        techniques.push(technique);
     }
 
-    let mut v = shuffle;
-    let mut n = n;
-    for (i, &power) in powers.iter().enumerate().rev() {
-        let p = 1 << i as Unsigned;
-        while n >= p {
-            v = apply(power, v, n_cards);
-            n -= p;
+    techniques
+}
+
+/// Represents a shuffle as a linear equation `v`, modulo `n_cards`
+/// Formula: `ax + b`; where `v = [a, b]`
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+struct Shuffle {
+    v: [Unsigned; 2],
+    n_cards: Unsigned,
+}
+
+impl Shuffle {
+    fn from_techniques(techniques: &[Technique], n_cards: Unsigned) -> Self {
+        // Since we're trying to find card `X` at position `x`,
+        // we need to follow it backwards through the shuffling techniques
+        let mut v = [1, 0];
+        for n in (0..techniques.len()).rev() {
+            let technique = techniques[n];
+
+            use Technique::*;
+            match technique {
+                DealNewStack => {
+                    v = Shuffle::undeal_new_stack(v, n_cards);
+                },
+                Cut(n) => {
+                    v = Shuffle::uncut(n, v, n_cards);
+                },
+                DealWithIncrement(n) => {
+                    v = Shuffle::undeal_with_increment(n, v, n_cards);
+                },
+            }
         }
+
+        Shuffle { v, n_cards }
     }
 
-    v
-}
+    /// Find the card at `pos`, given a `shuffle`
+    fn evaluate(&self, pos: Unsigned) -> Unsigned {
+        // Formula: ax + b;  where v = [a, b]
+        (self.v[0] * pos + self.v[1]) % self.n_cards
+    }
 
-/// Apply shuffle `[a, b]` to shuffle `[c, d]`
-fn apply([a, b]: Vector, [c, d]: Vector, n_cards: Unsigned) -> Vector {
-    // Substitute `x = cx + d` into `ax + b` => `a(cx + d) + b = acx + ad + b`
-    [(a * c) % n_cards, (a * d + b) % n_cards]
-}
-
-/// Shuffle `n_cards` using `techniques`
-fn shuffle(techniques: &[Technique], n_cards: Unsigned) -> Vector {
-    shuffle_(X, techniques, n_cards)
-}
-
-/// Shuffle `n_cards` using `techniques` on an existing `shuffle`
-fn shuffle_(mut shuffle: Vector, techniques: &[Technique], n_cards: Unsigned) -> Vector {
-    // Since we're trying to find card `X` at position `x`,
-    // we need to follow it backwards through the shuffling techniques
-    for n in (0..techniques.len()).rev() {
-        let technique = techniques[n];
-
-        use Technique::*;
-        match technique {
-            DealNewStack => {
-                shuffle = undeal_new_stack(shuffle, n_cards);
-            },
-            Cut(n) => {
-                shuffle = uncut(n, shuffle, n_cards);
-            },
-            DealWithIncrement(n) => {
-                shuffle = undeal_with_increment(n, shuffle, n_cards);
-            },
+    /// Repeat thus shuffle `n` times
+    fn repeat(&self, n: Unsigned) -> Shuffle {
+        // Calculate the powers-of-2 repeats less than `n`
+        let mut powers = vec![*self];
+        while 1 << (powers.len() as Unsigned - 1) < n {
+            // Apply the currently largest shuffle to itself
+            let shuffle = powers[powers.len() - 1];
+            powers.push(shuffle.apply(shuffle));
         }
+
+        let mut shuffle = *self;
+        let mut n = n;
+        for (i, &power) in powers.iter().enumerate().rev() {
+            let p = 1 << i as Unsigned;
+            while n >= p {
+                shuffle = power.apply(shuffle);
+                n -= p;
+            }
+        }
+
+        shuffle
     }
 
-    shuffle
-}
+    /// Apply this shuffle to `other` shuffle`
+    fn apply(&self, other: Shuffle) -> Shuffle {
+        assert_eq!(other.n_cards, self.n_cards, "Number of cards must be equal");
+        // Substitute `x = cx + d` into `ax + b` => `a(cx + d) + b = acx + ad + b`
+        let [a, b] = self.v;
+        let [c, d] = other.v;
+        let v = [(a * c) % self.n_cards, (a * d + b) % self.n_cards];
 
-/// Undo cut
-fn uncut(n: Signed, v: Vector, n_cards: Unsigned) -> Vector {
-    // i: 0  1  2  3  4  5  6  7  8  9
-    // | c1 c2 c3 c4 c5 c6 c7*c8 c9 c0 |  Cut 1
-    // | c0 c1 c2 c3 c4 c5 c6 c7*c8 c9 |
-    //
-    // Formula: x + n mod m
-    [modulo(v[0], n_cards), modulo(v[1] as Signed + n as Signed, n_cards)]
-}
+        Shuffle { v, n_cards: self.n_cards }
+    }
 
-/// Undo deal with increment
-fn undeal_with_increment(n: Unsigned, v: Vector, n_cards: Unsigned) -> Vector {
-    // i: 0  1  2  3  4  5  6  7  8  9
-    // | c0 c7*c4 c1 c8 c5 c2 c9 c6 c3 |  Deal with increment 3
-    // | c0 c1 c2 c3 c4 c5 c6 c7*c8 c9 |
-    //
-    // This requires us to find the modular multiplicative inverse
-    // Formula: n⁻¹x mod m
-    let ninv = inverse(n, n_cards);
-    [modulo(ninv * v[0], n_cards), modulo(ninv * v[1], n_cards)]
-}
+    /// Undo cut
+    fn uncut(n: Signed, [a, b]: Vector2, n_cards: Unsigned) -> Vector2 {
+        // i: 0  1  2  3  4  5  6  7  8  9
+        // | c1 c2 c3 c4 c5 c6 c7*c8 c9 c0 |  Cut 1
+        // | c0 c1 c2 c3 c4 c5 c6 c7*c8 c9 |
+        //
+        // Formula: x + n mod m
+        [modulo(a, n_cards), modulo(b as Signed + n as Signed, n_cards)]
+    }
 
-/// Undo deal new stack
-fn undeal_new_stack(v: Vector, n_cards: Unsigned) -> Vector {
-    // i: 0  1  2  3  4  5  6  7  8  9
-    // | c9 c8 c7*c6 c5 c4 c3 c2 c1 c0 |  Deal new stack
-    // | c0 c1 c2 c3 c4 c5 c6 c7*c8 c9 |
-    //
-    // Formula: -(x + 1) mod m
-    [modulo(-(v[0] as Signed), n_cards), modulo(-(v[1] as Signed) - 1, n_cards)]
+    /// Undo deal with increment
+    fn undeal_with_increment(n: Unsigned, [a, b]: Vector2, n_cards: Unsigned) -> Vector2 {
+        // i: 0  1  2  3  4  5  6  7  8  9
+        // | c0 c7*c4 c1 c8 c5 c2 c9 c6 c3 |  Deal with increment 3
+        // | c0 c1 c2 c3 c4 c5 c6 c7*c8 c9 |
+        //
+        // This requires us to find the modular multiplicative inverse
+        // Note: These multiplications may result in some *very* big numbers
+        //
+        // Formula: n⁻¹x mod m
+        let ninv = inverse(n, n_cards);
+        [modulo(ninv * a, n_cards), modulo(ninv * b, n_cards)]
+    }
+
+    /// Undo deal new stack
+    fn undeal_new_stack([a, b]: Vector2, n_cards: Unsigned) -> Vector2 {
+        // i: 0  1  2  3  4  5  6  7  8  9
+        // | c9 c8 c7*c6 c5 c4 c3 c2 c1 c0 |  Deal new stack
+        // | c0 c1 c2 c3 c4 c5 c6 c7*c8 c9 |
+        //
+        // Formula: -(x + 1) mod m
+        [modulo(-(a as Signed), n_cards), modulo(-(b as Signed) - 1, n_cards)]
+    }
 }
 
 /// Calculate `x mod m`
@@ -164,31 +202,6 @@ fn inverse(a: Unsigned, m: Unsigned) -> Unsigned {
     (m0 as Signed + x) as Unsigned % m0
 }
 
-fn read_input<T: AsRef<Path>>(path: T) -> Vec<Technique> {
-    let contents = fs::read_to_string(path).expect("Failed to read input");
-
-    let mut techniques = Vec::new();
-    for line in contents.lines() {
-        let technique = match line {
-            DEAL_NEW_STACK => Technique::DealNewStack,
-            line if line.starts_with(CUT) => {
-                let n = line[CUT.len()..].parse().expect("Failed to parse value");
-
-                Technique::Cut(n)
-            },
-            line if line.starts_with(DEAL_WITH_INCREMENT) => {
-                let n = line[DEAL_WITH_INCREMENT.len()..].parse().expect("Failed to parse value");
-
-                Technique::DealWithIncrement(n)
-            }
-            line => panic!("Unknown line: {}", line),
-        };
-
-        techniques.push(technique);
-    }
-
-    techniques
-}
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 enum Technique {
@@ -206,10 +219,9 @@ mod tests {
         let shuffled = &[9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
         let n_cards = shuffled.len() as Unsigned;
 
-        let v = shuffle(&[DealNewStack], n_cards);
+        let shuffle = Shuffle::from_techniques(&[DealNewStack], n_cards);
         for i in 0..n_cards {
-            let pos = evaluate(v, i, n_cards);
-            assert_eq!(shuffled[i as usize], pos);
+            assert_eq!(shuffle.evaluate(i), shuffled[i as usize]);
         }
     }
 
@@ -218,10 +230,9 @@ mod tests {
         let shuffled = &[1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
         let n_cards = shuffled.len() as Unsigned;
 
-        let v = shuffle(&[Cut(1)], n_cards);
+        let shuffle = Shuffle::from_techniques(&[Cut(1)], n_cards);
         for i in 0..n_cards {
-            let pos = evaluate(v, i, n_cards);
-            assert_eq!(shuffled[i as usize], pos);
+            assert_eq!(shuffle.evaluate(i), shuffled[i as usize]);
         }
     }
 
@@ -230,10 +241,9 @@ mod tests {
         let shuffled = &[3, 4, 5, 6, 7, 8, 9, 0, 1, 2];
         let n_cards = shuffled.len() as Unsigned;
 
-        let v= shuffle(&[Cut(3)], n_cards);
+        let shuffle = Shuffle::from_techniques(&[Cut(3)], n_cards);
         for i in 0..n_cards {
-            let pos = evaluate(v, i, n_cards);
-            assert_eq!(shuffled[i as usize], pos);
+            assert_eq!(shuffle.evaluate(i), shuffled[i as usize]);
         }
     }
 
@@ -242,22 +252,20 @@ mod tests {
         let shuffled = &[9, 0, 1, 2, 3, 4, 5, 6, 7, 8];
         let n_cards = shuffled.len() as Unsigned;
 
-        let v= shuffle(&[Cut(-1)], n_cards);
+        let shuffle = Shuffle::from_techniques(&[Cut(-1)], n_cards);
         for i in 0..n_cards {
-            let pos = evaluate(v, i, n_cards);
-            assert_eq!(shuffled[i as usize], pos);
+            assert_eq!(shuffle.evaluate(i), shuffled[i as usize]);
         }
     }
 
     #[test]
     fn test_negativecut4() {
-        let deck = &[6, 7, 8, 9, 0, 1, 2, 3, 4, 5];
-        let n_cards = deck.len() as Unsigned;
+        let shuffled = &[6, 7, 8, 9, 0, 1, 2, 3, 4, 5];
+        let n_cards = shuffled.len() as Unsigned;
 
-        let v= shuffle(&[Cut(-4)], n_cards);
+        let shuffle = Shuffle::from_techniques(&[Cut(-4)], n_cards);
         for i in 0..n_cards {
-            let pos = evaluate(v, i, n_cards);
-            assert_eq!(deck[i as usize], pos);
+            assert_eq!(shuffle.evaluate(i), shuffled[i as usize]);
         }
     }
 
@@ -266,10 +274,9 @@ mod tests {
         let shuffled = &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
         let n_cards = shuffled.len() as Unsigned;
 
-        let v= shuffle(&[DealWithIncrement(1)], n_cards);
+        let shuffle = Shuffle::from_techniques(&[DealWithIncrement(1)], n_cards);
         for i in 0..10 {
-            let pos = evaluate(v, i, n_cards);
-            assert_eq!(shuffled[i as usize], pos);
+            assert_eq!(shuffle.evaluate(i), shuffled[i as usize]);
         }
     }
 
@@ -278,10 +285,9 @@ mod tests {
         let shuffled = &[0, 7, 4, 1, 8, 5, 2, 9, 6, 3];
         let n_cards = shuffled.len() as Unsigned;
 
-        let v= shuffle(&[DealWithIncrement(3)], n_cards);
+        let shuffle = Shuffle::from_techniques(&[DealWithIncrement(3)], n_cards);
         for i in 0..n_cards {
-            let pos = evaluate(v, i, n_cards);
-            assert_eq!(shuffled[i as usize], pos);
+            assert_eq!(shuffle.evaluate(i), shuffled[i as usize]);
         }
     }
 
@@ -292,10 +298,9 @@ mod tests {
         let shuffled = [0, 3, 6, 9, 2, 5, 8, 1, 4, 7];
         let n_cards = shuffled.len() as Unsigned;
 
-        let v= shuffle(&SAMPLE_1, n_cards);
+        let shuffle = Shuffle::from_techniques(&SAMPLE_1, n_cards);
         for i in 0..n_cards {
-            let pos = evaluate(v, i, n_cards);
-            assert_eq!(shuffled[i as usize], pos);
+            assert_eq!(shuffle.evaluate(i), shuffled[i as usize]);
         }
     }
 
@@ -306,10 +311,9 @@ mod tests {
         let shuffled = [3, 0, 7, 4, 1, 8, 5, 2, 9, 6];
         let n_cards = shuffled.len() as Unsigned;
 
-        let v= shuffle(&SAMPLE_2, n_cards);
+        let shuffle = Shuffle::from_techniques(&SAMPLE_2, n_cards);
         for i in 0..n_cards {
-            let pos = evaluate(v, i, n_cards);
-            assert_eq!(shuffled[i as usize], pos);
+            assert_eq!(shuffle.evaluate(i), shuffled[i as usize]);
         }
     }
 
@@ -320,44 +324,39 @@ mod tests {
         let n_cards = 10;
         for i in 0..10 {
             // Cut n
-            assert_eq!((i + 7) % n_cards, evaluate(shuffle(&[Cut(7)], n_cards), i, n_cards));
-            assert_eq!(evaluate(uncut(7, X, n_cards), i, n_cards),
-                       evaluate(shuffle(&[Cut(7)], n_cards), i, n_cards));
+            assert_eq!((i + 7) % n_cards, Shuffle::from_techniques(&[Cut(7)], n_cards).evaluate(i));
 
             // DealWithIncrement m
-            assert_eq!((inverse(7, n_cards) * i) % n_cards, evaluate(shuffle(&[DealWithIncrement(7)], n_cards), i, n_cards));
-            assert_eq!(evaluate(undeal_with_increment(7, X, n_cards), i, n_cards),
-                       evaluate(shuffle(&[DealWithIncrement(7)], n_cards), i, n_cards));
+            assert_eq!((inverse(7, n_cards) * i) % n_cards, Shuffle::from_techniques(&[DealWithIncrement(7)], n_cards).evaluate(i));
 
             // DealNewStack
-            assert_eq!((n_cards - i - 1) % n_cards, evaluate(shuffle(&[DealNewStack], n_cards), i, n_cards));
-            assert_eq!(evaluate(undeal_new_stack(X, n_cards), i, n_cards),
-                       evaluate(shuffle(&[DealNewStack], n_cards), i, n_cards));
+            assert_eq!((n_cards - i - 1) % n_cards, Shuffle::from_techniques(&[DealNewStack], n_cards).evaluate(i));
         }
     }
 
     #[test]
     fn test_repeat0() {
-        assert_eq!(shuffle(&SAMPLE_4, M_CARDS),
-                   repeat(shuffle(&SAMPLE_4, M_CARDS), 0, M_CARDS));
+        assert_eq!(Shuffle::from_techniques(&SAMPLE_4, M_CARDS),
+                   Shuffle::from_techniques(&SAMPLE_4, M_CARDS).repeat(0));
     }
 
     #[test]
     fn test_repeat1() {
         let repeated: Vec<_> = SAMPLE_4.iter().copied().cycle().take(2 * SAMPLE_4.len()).collect();  // Repeated once
 
-        assert_eq!(shuffle(&repeated, M_CARDS),
-                   shuffle_(shuffle(&SAMPLE_4, M_CARDS), &SAMPLE_4, M_CARDS));
+        assert_eq!(Shuffle::from_techniques(&repeated, M_CARDS),
+                   Shuffle::from_techniques(&SAMPLE_4, M_CARDS)
+                       .apply(Shuffle::from_techniques(&SAMPLE_4, M_CARDS)));
 
-        assert_eq!(shuffle(&repeated, M_CARDS),
-                   repeat(shuffle(&SAMPLE_4, M_CARDS), 1, M_CARDS));
+        assert_eq!(Shuffle::from_techniques(&repeated, M_CARDS),
+                   Shuffle::from_techniques(&SAMPLE_4, M_CARDS).repeat(1));
     }
 
     #[test]
     fn test_repeat4() {
         let repeated: Vec<_> = SAMPLE_4.iter().copied().cycle().take(5 * SAMPLE_4.len()).collect();  // Repeated 4 times
 
-        assert_eq!(shuffle(&repeated, M_CARDS),
-                   repeat(shuffle(&SAMPLE_4, M_CARDS), 4, M_CARDS));
+        assert_eq!(Shuffle::from_techniques(&repeated, M_CARDS),
+                   Shuffle::from_techniques(&SAMPLE_4, M_CARDS).repeat(4));
     }
 }
