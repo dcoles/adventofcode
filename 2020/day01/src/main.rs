a6018b625d16dfc9fe3e198b3e4b302d36f1aea5
David Coles <coles.david@gmail.com>
2020-12-01 08:28:49 -0800

    2020: Day 1 - Don't compare entries to themselves
    We can also simplify the program by just panicing if we can't
    find the result.

--- 2020/day01/src/main.rs
+++ 2020/day01/src/main.rs
@@ -7,8 +7,8 @@ const TARGET: i32 = 2020;
 fn main() {
     let input = read_input("input.txt");
 
-    println!("Part 1: {}", part1(&input).expect("No result?"));
-    println!("Part 2: {}", part2(&input).expect("No result?"));
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 fn read_input<T: AsRef<Path>>(path: T) -> Vec<i32> {
@@ -18,28 +18,32 @@ fn read_input<T: AsRef<Path>>(path: T) -> Vec<i32> {
         .collect()
 }
 
-fn part1(input: &[i32]) -> Option<i32> {
-    for i in input {
-        for j in input {
-            if i + j == TARGET {
-                return Some(i * j);
+fn part1(input: &[i32]) -> i32 {
+    // We don't need to compare items to themselves
+    // or other combinations we've already tried
+    for (i, entry1) in input.iter().enumerate() {
+        for entry2 in input.iter().skip(i + 1) {
+            if entry1 + entry2 == TARGET {
+                return entry1 * entry2;
             }
         }
     }
 
-    None
+    panic!("No result found!");
 }
 
-fn part2(input: &[i32]) -> Option<i32> {
-    for i in input {
-        for j in input {
-            for k in input {
-                if i + j + k == TARGET {
-                    return Some(i * j * k);
+fn part2(input: &[i32]) -> i32 {
+    // We don't need to compare items to themselves
+    // or other combinations we've already tried
+    for (i, entry1) in input.iter().enumerate() {
+        for (j, entry2) in input.iter().skip(i + 1).enumerate() {
+            for entry3 in input.iter().skip(i + j + 2) {
+                if entry1 + entry2 + entry3 == TARGET {
+                    return entry1 * entry2 * entry3;
                 }
             }
         }
     }
 
-    None
+    panic!("No result found!");
 }
