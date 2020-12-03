f7bba7c20884fbfb41397949fa45452aa65d013e
David Coles <coles.david@gmail.com>
2020-12-02 21:51:47 -0800

    2020: Update template

--- 2020/template/src/main.rs
+++ 2020/template/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
 fn read_input<T: AsRef<Path>>(path: T) -> Vec<i32> {
     fs::read_to_string(path).expect("Failed to read input")
         .lines()
-        .map(|line| line.parse::<i32>().expect("Failed to parse line"))
+        .map(|line| line.parse().expect("Failed to parse line"))
         .collect()
 }
 
@@ -21,12 +21,6 @@ mod tests {
     fn test_part1_example1() {
         let input = read_input("input.txt");
         assert_eq!(input[0], 1);
-    }
-
-    #[test]
-    fn test_part2_example1() {
-        let input = read_input("input.txt");
-        assert_eq!(input[1], 2);
     }
 }
 
