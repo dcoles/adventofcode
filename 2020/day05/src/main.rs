e3aaed929c96cd5ee81a2a2c36dc08958fa882b7
David Coles <coles.david@gmail.com>
2020-12-04 21:56:13 -0800

    2020: Day 5 - Part 2

--- 2020/day05/src/main.rs
+++ 2020/day05/src/main.rs
@@ -1,5 +1,9 @@
 use std::path::Path;
 use std::fs;
+use std::collections::HashSet;
+
+const ROWS_POW: usize = 7;  // 128 rows
+const COLS_POW: usize = 3;  // 8 columns
 
 const FRONT: char = 'F';
 const BACK: char = 'B';
@@ -8,9 +12,12 @@ const RIGHT: char = 'R';
 
 fn main() {
     let input = read_input("input.txt");
-    let max_seatid = input.iter().map(|s| seatid(&s)).max().unwrap();
+    let seatids: HashSet<_> = input.iter().map(|s| seatid(s)).collect();
+    let max_seatid = seatids.iter().max().unwrap().clone();
+    let your_seatid = find_seat(&seatids, max_seatid);
 
     println!("Part 1: Max Seat ID is {}", max_seatid);
+    println!("Part 2: Your Seat ID is {}", your_seatid);
 }
 
 fn read_input<T: AsRef<Path>>(path: T) -> Vec<String> {
@@ -21,10 +28,12 @@ fn read_input<T: AsRef<Path>>(path: T) -> Vec<String> {
 }
 
 fn seatid(seq: &str) -> u32 {
-    let row = partition(&seq[..7]);
-    let column = partition(&seq[7..]);
+    assert_eq!(seq.len(), ROWS_POW + COLS_POW);
 
-    row * 8 + column
+    let row = partition(&seq[..ROWS_POW]);
+    let column = partition(&seq[ROWS_POW..]);
+
+    row * 2_u32.pow(COLS_POW as u32) + column
 }
 
 fn partition(seq: &str) -> u32 {
@@ -36,11 +45,25 @@ fn partition(seq: &str) -> u32 {
         match c {
             FRONT | LEFT => max = mid,
             BACK | RIGHT => min = mid + 1,
-            _ => panic!("Unknown character"),
+            _ => panic!("Unknown character {}", c),
         }
     }
 
     min
+}
+
+fn find_seat(seatids: &HashSet<u32>, max_seatid: u32) -> u32 {
+    for i in 1..(max_seatid-1) {
+        // Must be an empty seat...
+        if !seatids.contains(&i) {
+            // ...with seats on either side
+            if seatids.contains(&(i-1)) && seatids.contains(&(i+1)) {
+                return i;
+            }
+        }
+    }
+
+    panic!("Could not find seat!")
 }
 
 #[cfg(test)]
@@ -50,7 +73,7 @@ mod tests {
     #[test]
     fn test_partition() {
         let seq = "FBFBBFFRLR";
-        assert_eq!(partition(&seq[..7]), 44);
-        assert_eq!(partition(&seq[7..]), 5);
+        assert_eq!(partition(&seq[..ROWS_POW]), 44);
+        assert_eq!(partition(&seq[ROWS_POW..]), 5);
     }
 }
