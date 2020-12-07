178fa9f26259655196089de649e2ba359350e572
David Coles <coles.david@gmail.com>
2020-12-07 13:55:43 -0800

    2020: Extend template with more complete example

--- 2020/template/src/main.rs
+++ 2020/template/src/main.rs
@@ -1,16 +1,33 @@
 use std::path::Path;
 use std::fs;
+use regex::Regex;
+
+type Input = Vec<String>;
 
 fn main() {
     let input = read_input("input.txt");
-    println!("> {:?}", input);
+
+    println!("Part 1: {}", part1(&input));
 }
 
-fn read_input<T: AsRef<Path>>(path: T) -> Vec<i32> {
+fn read_input<T: AsRef<Path>>(path: T) -> Input {
     fs::read_to_string(path).expect("Failed to read input")
         .lines()
-        .map(|line| line.parse().expect("Failed to parse line"))
+        .map(|line| line.to_owned())
         .collect()
+}
+
+fn part1(input: &Input) -> u32 {
+    let re = Regex::new(r"^(\d+)$").unwrap();
+
+    let mut sum = 0;
+    for line in input {
+        if let Some(m) = re.captures(&line) {
+            sum += m[1].parse::<u32>().expect("Failed to parse number");
+        }
+    }
+
+    sum
 }
 
 #[cfg(test)]
@@ -18,9 +35,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part1_example1() {
+    fn test_part1() {
         let input = read_input("input.txt");
-        assert_eq!(input[0], 1);
+        assert_eq!(part1(&input), 45);
     }
 }
 
