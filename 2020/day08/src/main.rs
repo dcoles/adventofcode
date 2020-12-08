89ef432849f460a6699cfd8e044efc35e91bb869
David Coles <coles.david@gmail.com>
2020-12-07 22:48:42 -0800

    2020: Day 8 - Cleanup

--- 2020/day08/src/main.rs
+++ 2020/day08/src/main.rs
@@ -5,10 +5,10 @@ use std::collections::HashSet;
 type Program = Vec<Instruction>;
 
 fn main() {
-    let program = read_input("input.txt");
+    let input = read_input("input.txt");
 
-    println!("Part 1: {}", part1(&program));
-    println!("Part 2: {}", part2(&program));
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 fn read_input<T: AsRef<Path>>(path: T) -> Program {
@@ -26,18 +26,17 @@ fn read_input<T: AsRef<Path>>(path: T) -> Program {
         .collect()
 }
 
+/// Run program until just before an instruction would be executed a second time,
+/// then return value of the accumulator.
 fn part1(input: &Program) -> i32 {
     let mut cpu = CPU::from_program(input);
-
-    let mut seen = HashSet::new();
-    while !seen.contains(&cpu.pc) {
-        seen.insert(cpu.pc);
-        cpu.step();
-    }
+    run_until_loop(&mut cpu);
 
     cpu.acc
 }
 
+/// Mutate NOP/JMP instructions in a program until it exits cleanly,
+/// then return value of the accumulator.
 fn part2(input: &Program) -> i32 {
     for i in 0..input.len() {
         let mut program = input.clone();
@@ -45,22 +44,27 @@ fn part2(input: &Program) -> i32 {
         match program[i].op {
             Operation::NOP => program[i].op = Operation::JMP,
             Operation::JMP => program[i].op = Operation::NOP,
-            _ => (),
+            _ => continue,
         }
 
         let mut cpu = CPU::from_program(&program);
-        let mut seen = HashSet::new();
-        while !seen.contains(&cpu.pc) && cpu.pc < cpu.program.len() {
-            seen.insert(cpu.pc);
-            cpu.step();
-        }
+        run_until_loop(&mut cpu);
 
-        if cpu.pc == program.len() {
+        if cpu.is_eof() {
             return cpu.acc;
         }
     }
 
     panic!("No solution found!")
+}
+
+/// Step through program instructions until EOF or program would re-execute an instruction.
+fn run_until_loop(cpu: &mut CPU) {
+    let mut seen = HashSet::new();
+    while !cpu.is_eof() && !seen.contains(&cpu.pc) {
+        seen.insert(cpu.pc);
+        cpu.step().expect("Execution failed");
+    }
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -98,17 +102,27 @@ impl<'a> CPU<'a> {
         CPU { pc: 0, acc: 0, program }
     }
 
-    fn step(&mut self) {
+    fn is_eof(&self) -> bool {
+        self.pc == self.program.len()
+    }
+
+    fn step(&mut self) -> Result<(), &'static str> {
+        if self.pc >= self.program.len() {
+            return Err("Attempt to execute out of bounds");
+        }
+
         let instr = self.program[self.pc];
         match instr.op {
             Operation::NOP => (),
             Operation::JMP => {
                 self.pc = (self.pc as i32 + instr.arg) as usize;
-                return;
+                return Ok(());
             },
             Operation::ACC => self.acc += instr.arg,
         }
+
         self.pc += 1;
+        Ok(())
     }
 }
 
