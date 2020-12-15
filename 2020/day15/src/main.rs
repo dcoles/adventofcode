b0d56f04d54af105fae68923ee83625b1a72864e
David Coles <coles.david@gmail.com>
2020-12-14 22:00:42 -0800

    2020: Day 15 - Part 2

--- 2020/day15/src/main.rs
+++ 2020/day15/src/main.rs
@@ -1,12 +1,15 @@
 use std::path::Path;
 use std::fs;
+use std::collections::HashMap;
 
 type Input = Vec<usize>;
 
 fn main() {
     let input = read_input("input.txt");
 
-    println!("Part 1: {}", part1(&input, 2020));
+    println!("Part 1: {}", recite(&input, 2020));
+    println!("Part 2: {}", recite(&input, 30000000));
+    recite(&[0, 3, 6], 2020);
 }
 
 fn read_input<T: AsRef<Path>>(path: T) -> Input {
@@ -17,19 +20,31 @@ fn read_input<T: AsRef<Path>>(path: T) -> Input {
         .collect()
 }
 
-fn part1(input: &[usize], max_n: usize) -> usize {
-    let mut numbers: Vec<_> = input.to_owned();
-    for _ in input.len()+1..=max_n {
-        let &prev = numbers.last().unwrap();
+fn recite(input: &[usize], max_n: usize) -> usize {
+    // Map of numbers to the last time that number was used
+    let mut map: HashMap<usize, Vec<usize>> = HashMap::new();
 
-        if let Some(difference) = numbers.iter().rev().skip(1).position(|&x| x == prev) {
-            numbers.push(difference + 1);
-        } else {
-            numbers.push(0);
-        }
+    // Insert the starting numbers
+    let mut last = 0;
+    for (n, &x) in input.into_iter().enumerate() {
+        map.entry(x).or_default().push(n + 1);
+        last = x;
     }
 
-    *numbers.last().unwrap()
+    // Index of previous number
+    for n in input.len()..max_n {
+        // Find the index of the second last time this number was used
+        let &m = map.get(&last)
+            .and_then(|xs| xs.iter().rev().skip(1).next())
+            .unwrap_or(&n);
+
+        let difference = n - m;
+
+        map.entry(difference).or_default().push(n + 1);
+        last = difference;
+    }
+
+    last
 }
 
 #[cfg(test)]
@@ -38,7 +53,7 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(&[0, 3, 6], 2020), 436);
+        assert_eq!(recite(&[0, 3, 6], 2020), 436);
     }
 }
 
