f6553db508620fdd327b48c9b64f6453d074a982
David Coles <coles.david@gmail.com>
2020-12-16 22:10:26 -0800

    2020: Day 17 - Part 2

--- 2020/day17/src/main.rs
+++ 2020/day17/src/main.rs
@@ -6,14 +6,24 @@ fn main() {
     let map = Map::from_file("input.txt");
 
     println!("Part 1: {}", part1(&map));
+    println!("Part 2: {}", part2(&map));
 }
 
 fn part1(map: &Map) -> usize {
     let mut map = map.clone();
 
     for _ in 0..6 {
-        map.tick();
-        map.print(0);
+        map.tick3();
+    }
+
+    map.active_positions().len()
+}
+
+fn part2(map: &Map) -> usize {
+    let mut map = map.clone();
+
+    for _ in 0..6 {
+        map.tick4();
     }
 
     map.active_positions().len()
@@ -21,7 +31,7 @@ fn part1(map: &Map) -> usize {
 
 const INACTIVE: char = '.';
 const ACTIVE: char = '#';
-type Pos = (i32, i32, i32);
+type Pos = (i32, i32, i32, i32);
 
 #[derive(Clone)]
 struct Map {
@@ -33,13 +43,13 @@ struct Map {
 impl Map {
     fn from_file<T: AsRef<Path>>(path: T) -> Self {
         let mut tiles = BTreeMap::new();
-        let min = (0, 0, 0);
-        let mut max = (0, 0, 0);
+        let min = (0, 0, 0, 0);
+        let mut max = (0, 0, 0, 0);
         let input = fs::read_to_string(path).expect("Failed to read input");
         for (y, line) in input.lines().enumerate() {
             for (x, char) in line.chars().enumerate() {
-                tiles.insert((x as i32, y as i32, 0), char == ACTIVE);
-                max = (max.0.max(x as i32), max.1.max(y as i32), 0);
+                tiles.insert((x as i32, y as i32, 0, 0), char == ACTIVE);
+                max = (max.0.max(x as i32), max.1.max(y as i32), 0, 0);
             }
         }
 
@@ -52,26 +62,51 @@ impl Map {
 
     fn active(&mut self, pos: Pos, active: bool) {
         self.tiles.insert(pos, active);
-        self.min = (self.min.0.min(pos.0), self.min.1.min(pos.1), self.min.2.min(pos.2));
-        self.max = (self.max.0.max(pos.0), self.max.1.max(pos.1), self.max.2.max(pos.2));
+        self.min = (self.min.0.min(pos.0), self.min.1.min(pos.1), self.min.2.min(pos.2), self.min.3.min(pos.3));
+        self.max = (self.max.0.max(pos.0), self.max.1.max(pos.1), self.max.2.max(pos.2), self.max.3.max(pos.3));
     }
 
-    fn tick(&mut self) {
+    fn tick3(&mut self) {
         let cur = self.tiles.clone();
 
         for x in self.min.0-1..=self.max.0+1 {
             for y in self.min.1 - 1..=self.max.1 + 1 {
                 for z in self.min.2 - 1..=self.max.2 + 1 {
-                    let active = self.is_active((x, y, z));
-                    let neighbours = adjacent((x, y, z));
+                    let active = self.is_active((x, y, z, 0));
+                    let neighbours = adjacent3((x, y, z, 0));
                     let n_active = neighbours.iter().filter(|&p| cur.get(p).copied().unwrap_or(false)).count();
                     if active {
                         if !(n_active == 2 || n_active == 3) {
-                            self.active((x, y, z), false);
+                            self.active((x, y, z, 0), false);
                         }
                     } else {
                         if n_active == 3 {
-                            self.active((x, y, z), true);
+                            self.active((x, y, z, 0), true);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn tick4(&mut self) {
+        let cur = self.tiles.clone();
+
+        for x in self.min.0-1..=self.max.0+1 {
+            for y in self.min.1 - 1..=self.max.1 + 1 {
+                for z in self.min.2 - 1..=self.max.2 + 1 {
+                    for w in self.min.3 - 1..=self.max.3 + 1 {
+                        let active = self.is_active((x, y, z, w));
+                        let neighbours = adjacent4((x, y, z, w));
+                        let n_active = neighbours.iter().filter(|&p| cur.get(p).copied().unwrap_or(false)).count();
+                        if active {
+                            if !(n_active == 2 || n_active == 3) {
+                                self.active((x, y, z, w), false);
+                            }
+                        } else {
+                            if n_active == 3 {
+                                self.active((x, y, z, w), true);
+                            }
                         }
                     }
                 }
@@ -83,10 +118,10 @@ impl Map {
         self.tiles.iter().filter(|(_, active)| **active).map(|(pos, _)| *pos).collect()
     }
 
-    fn print(&self, z: i32) {
+    fn print(&self, z: i32, w: i32) {
         for y in self.min.1..=self.max.1 {
             for x in self.min.0..=self.max.0 {
-                print!("{}", if self.is_active((x, y, z)) { ACTIVE } else { INACTIVE });
+                print!("{}", if self.is_active((x, y, z, w)) { ACTIVE } else { INACTIVE });
             }
             println!();
         }
@@ -94,13 +129,30 @@ impl Map {
     }
 }
 
-fn adjacent(pos: Pos) -> Vec<Pos> {
+fn adjacent3(pos: Pos) -> Vec<Pos> {
     let mut positions = Vec::new();
     for x in pos.0-1..=pos.0+1 {
         for y in pos.1-1..=pos.1+1 {
             for z in pos.2-1..=pos.2+1 {
-                if (x, y, z) != pos {
-                    positions.push((x, y, z));
+                if (x, y, z, 0) != pos {
+                    positions.push((x, y, z, 0));
+                }
+            }
+        }
+    }
+
+    positions
+}
+
+fn adjacent4(pos: Pos) -> Vec<Pos> {
+    let mut positions = Vec::new();
+    for x in pos.0-1..=pos.0+1 {
+        for y in pos.1-1..=pos.1+1 {
+            for z in pos.2-1..=pos.2+1 {
+                for w in pos.3-1..=pos.3+1 {
+                    if (x, y, z, w) != pos {
+                        positions.push((x, y, z, w));
+                    }
                 }
             }
         }
@@ -114,19 +166,27 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_adjacent() {
-        let adj = adjacent((0, 0, 0));
+    fn test_adjacent3() {
+        let adj = adjacent3((0, 0, 0, 0));
         assert_eq!(adj.len(), 26);
     }
 
     #[test]
-    fn test_sample1() {
-        let mut map = Map::from_file("sample1.txt");
-        for _ in 0..6 {
-            map.tick();
-            map.print(0);
-        }
-        assert_eq!(map.active_positions().len(), 112);
+    fn test_adjacent4() {
+        let adj = adjacent4((0, 0, 0, 0));
+        assert_eq!(adj.len(), 80);
+    }
+
+    #[test]
+    fn test_sample1_part1() {
+        let map = Map::from_file("sample1.txt");
+        assert_eq!(part1(&map), 112);
+    }
+
+    #[test]
+    fn test_sample1_part2() {
+        let map = Map::from_file("sample1.txt");
+        assert_eq!(part2(&map), 848);
     }
 }
 
