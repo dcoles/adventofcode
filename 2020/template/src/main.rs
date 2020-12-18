6e567ab61a141b8d0c5c11fac79b15e297a3c844
David Coles <coles.david@gmail.com>
2020-12-18 13:54:42 -0800

    2020: Update template

--- 2020/template/src/main.rs
+++ 2020/template/src/main.rs
@@ -1,11 +1,11 @@
 use std::path::Path;
 use std::fs;
-use regex::Regex;
 
 type Input = Vec<String>;
 
 fn main() {
     let input = read_input("input.txt");
+    println!("> {:?}", input);
 
     println!("Part 1: {}", part1(&input));
 }
@@ -17,17 +17,8 @@ fn read_input<T: AsRef<Path>>(path: T) -> Input {
         .collect()
 }
 
-fn part1(input: &Input) -> u32 {
-    let re = Regex::new(r"^(\d+)$").unwrap();
-
-    let mut sum = 0;
-    for line in input {
-        if let Some(m) = re.captures(&line) {
-            sum += m[1].parse::<u32>().expect("Failed to parse number");
-        }
-    }
-
-    sum
+fn part1(input: &Input) -> i64 {
+    0
 }
 
 #[cfg(test)]
@@ -36,8 +27,8 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let input = read_input("input.txt");
-        assert_eq!(part1(&input), 45);
+        let input = read_input("sample1.txt");
+        assert_eq!(part1(&input), 0);
     }
 }
 
