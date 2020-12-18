8222d8129d456ee8a72b36f9e0599fbea59bf5c3
David Coles <coles.david@gmail.com>
2020-12-18 13:43:20 -0800

    2020: Day 18 - Part 2

--- 2020/day18/src/main.rs
+++ 2020/day18/src/main.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 use std::fs;
+use std::iter::Peekable;
 
 type Token = char;
 type Expr = Vec<Token>;
@@ -9,6 +10,7 @@ fn main() {
     let input = read_input("input.txt");
 
     println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 fn read_input<T: AsRef<Path>>(path: T) -> Vec<Expr> {
@@ -19,6 +21,10 @@ fn read_input<T: AsRef<Path>>(path: T) -> Vec<Expr> {
 
 fn part1(input: &Input) -> u64 {
     input.iter().map(eval).sum()
+}
+
+fn part2(input: &Input) -> u64 {
+    input.iter().map(eval2).sum()
 }
 
 fn parse(s: &str) -> Vec<Token> {
@@ -62,6 +68,69 @@ where
     lhs
 }
 
+fn eval2<'a, I>(tokens: I) -> u64
+    where
+        I: IntoIterator<Item=&'a Token>
+{
+    let mut tokens = tokens.into_iter().peekable();
+    let result = eval_m(&mut tokens);
+    assert!(tokens.next().is_none(), "Not all input consumed");
+
+    result
+}
+
+fn eval_m<'a, I>(tokens: &mut Peekable<I>) -> u64
+    where
+        I: Iterator<Item=&'a Token>
+{
+    let mut lhs = eval_a(tokens);
+
+    while let Some(&&op) = tokens.peek() {
+        if op != '*' {
+            return lhs;
+        }
+
+        tokens.next();
+        lhs *= eval_a(tokens);
+    }
+
+    lhs
+}
+
+fn eval_a<'a, I>(tokens: &mut Peekable<I>) -> u64
+    where
+        I: Iterator<Item=&'a Token>
+{
+    let mut lhs = eval_v(tokens);
+
+    while let Some(&&op) = tokens.peek() {
+        if op != '+' {
+            return lhs;
+        }
+
+        tokens.next();
+        lhs += eval_a(tokens);
+    }
+
+    lhs
+
+}
+
+fn eval_v<'a, I>(tokens: &mut Peekable<I>) -> u64
+    where
+        I: Iterator<Item=&'a Token>
+{
+    match *tokens.next().expect("Expected VALUE") {
+        '(' => {
+            let m = eval_m(tokens);
+            assert_eq!(')', *tokens.next().expect("Missing closing )"));
+            m
+        },
+        t if t.is_numeric() => t.to_digit(10).unwrap() as u64,
+        x => panic!("Unexpected token: {}", x),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -69,31 +138,37 @@ mod tests {
     #[test]
     fn test_example1() {
         assert_eq!(eval(&parse("1 + 2 * 3 + 4 * 5 + 6")), 71);
+        assert_eq!(eval2(&parse("1 + 2 * 3 + 4 * 5 + 6")), 231);
     }
 
     #[test]
     fn test_example2() {
         assert_eq!(eval(&parse("1 + (2 * 3) + (4 * (5 + 6))")), 51);
+        assert_eq!(eval2(&parse("1 + (2 * 3) + (4 * (5 + 6))")), 51);
     }
 
     #[test]
     fn test_example3() {
         assert_eq!(eval(&parse("2 * 3 + (4 * 5)")), 26);
+        assert_eq!(eval2(&parse("2 * 3 + (4 * 5)")), 46);
     }
 
     #[test]
     fn test_example4() {
-        assert_eq!(eval(&parse("5 + (8 * 3 + 9 + 3 * 4 * 3")), 437);
+        assert_eq!(eval(&parse("5 + (8 * 3 + 9 + 3 * 4 * 3)")), 437);
+        assert_eq!(eval2(&parse("5 + (8 * 3 + 9 + 3 * 4 * 3)")), 1445);
     }
 
     #[test]
     fn test_example5() {
         assert_eq!(eval(&parse("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")), 12240);
+        assert_eq!(eval2(&parse("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")), 669060);
     }
 
     #[test]
     fn test_example6() {
         assert_eq!(eval(&parse("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")), 13632);
+        assert_eq!(eval2(&parse("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")), 23340);
     }
 }
 
