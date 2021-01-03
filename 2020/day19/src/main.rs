35e75f91a23ccb0c70bde48ea00dab8fb09b9fc6
David Coles <coles.david@gmail.com>
2021-01-02 17:06:41 -0800

    2020: Day 19 - Part 2

--- 2020/day19/src/main.rs
+++ 2020/day19/src/main.rs
@@ -1,11 +1,71 @@
 use std::path::Path;
 use std::fs;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use regex::Regex;
 
 fn main() {
     let input = read_input("input.txt");
-    println!("Part 1: {}", input.count_matches(0));
+    println!("Part 1: {}", part1(&input));
+
+    let input = read_input("input2.txt");
+    println!("Part 2: {}", part2(&input));
+}
+
+fn part1(input: &Input) -> usize {
+    // Rule 0: 8 11 -> 42 42 31
+    let set42 = input.expand(42);
+    let n = set42.iter().next().unwrap().len();
+    let set31 = input.expand(31);
+
+    input.text.iter()
+        .filter(|t| matches(&set42, &t[..2*n], 2) && matches(&set31, &t[2*n..], 1))
+        .count()
+}
+
+fn matches(set: &HashSet<String>, s: &str, n: usize) -> bool {
+    if n == 0 {
+        return s == "";
+    }
+
+    for x in set {
+        let len = x.len();
+        if s.len() >= len && &s[..len] == x && matches(set, &s[len..], n - 1) {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn part2(input: &Input) -> usize {
+    // Rule 0: 8 11 -> 42+ 42(42(...)31)31
+    let set42 = input.expand(42);
+    let n = set42.iter().next().unwrap().len();
+    let set31 = input.expand(31);
+
+    input.text.iter()
+        .filter(|s| {
+            for n31 in 1.. {
+                if n31 * n > s.len() {
+                    // Too long
+                    break;
+                }
+
+                // Must match 42 more times than 31
+                for n42 in n31 + 1.. {
+                    if (n31 + n42) * n > s.len() {
+                        // Too long
+                        break;
+                    }
+
+                    if matches(&set42, &s[..n42*n], n42) && matches(&set31, &s[n42*n..], n31) {
+                        return true;
+                    }
+                }
+            }
+
+            false
+        }).count()
 }
 
 fn read_input<T: AsRef<Path>>(path: T) -> Input {
@@ -35,31 +95,30 @@ struct Input {
 }
 
 impl Input {
-    fn count_matches(&self, rule: u32) -> usize {
-        self.text.iter().filter(|t| self.match_rule(t, rule) == Some(t.as_str())).count()
-    }
+    /// Expand rule into all possible matches
+    fn expand(&self, id: u32) -> HashSet<String> {
+        let rule = self.rules.get(&id).expect("Unknown rule");
 
-    fn match_rule<'a>(&self, s: &'a str, rule: u32) -> Option<&'a str> {
-        let rule = self.rules.get(&rule).expect("Unknown rule");
         match rule {
-            Rule::Literal(c) => {
-                if s.starts_with(*c) { Some(&s[..1]) } else { None }
-            },
-            Rule::Subrule(srs) => {
-                'outer: for sr in srs {
-                    let mut i = 0;
-                    for &r in sr {
-                        if let Some(m) = self.match_rule(&s[i..], r) {
-                            i += m.len();
-                        } else {
-                            continue 'outer;
-                        }
-                    }
+            Rule::Literal(c) => [c.to_string()].iter().cloned().collect(),
+            Rule::Subrule(subrule) => {
+                let mut expanded = HashSet::new();
 
-                    return Some(&s[..i]);
+                for rs in subrule {
+                    match rs.len() {
+                        1 => expanded.extend(self.expand(rs[0])),
+                        2 => {
+                            for a in self.expand(rs[0]) {
+                                for b in self.expand(rs[1]) {
+                                    expanded.insert(a.clone() + b.as_str());
+                                }
+                            }
+                        }
+                        _ => panic!("Unsupported number of terms: {}", rs.len()),
+                    }
                 }
 
-                None
+                expanded
             }
         }
     }
@@ -76,7 +135,7 @@ impl Rule {
         use Rule::*;
 
         if s.starts_with('"') {
-            Literal(s.chars().skip(1).next().unwrap())
+            Literal(s.chars().nth(1).unwrap())
         } else {
             Subrule(
                 s.split('|')
@@ -85,25 +144,26 @@ impl Rule {
                     }).collect())
         }
     }
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_part1() {
-        let input = read_input("sample1.txt");
-        assert_eq!(input.match_rule("aaaabb", 0), Some("aaaabb"));
-        assert_eq!(input.match_rule("aaabab", 0), Some("aaabab"));
-        assert_eq!(input.match_rule("abbabb", 0), Some("abbabb"));
-        assert_eq!(input.match_rule("abbbab", 0), Some("abbbab"));
-        assert_eq!(input.match_rule("aabaab", 0), Some("aabaab"));
-        assert_eq!(input.match_rule("aabbbb", 0), Some("aabbbb"));
-        assert_eq!(input.match_rule("abaaab", 0), Some("abaaab"));
-        assert_eq!(input.match_rule("ababbb", 0), Some("ababbb"));
+    fn to_hashset(values: &[&str]) -> HashSet<String> {
+        values.iter().map(|s| s.to_string()).collect()
+    }
 
-        assert_eq!(input.count_matches(0), 2);
+    #[test]
+    fn test_expand() {
+        let input = read_input("sample1.txt");
+
+        assert_eq!(input.expand(5), to_hashset(&["b"]));
+        assert_eq!(input.expand(4), to_hashset(&["a"]));
+        assert_eq!(input.expand(3), to_hashset(&["ab", "ba"]));
+        assert_eq!(input.expand(2), to_hashset(&["aa", "bb"]));
+        assert_eq!(input.expand(1), to_hashset(&["aaab", "aaba", "bbab", "bbba", "abaa", "abbb", "baaa", "babb"]));
     }
 }
 
