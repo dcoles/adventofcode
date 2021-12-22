5670a950d1b62713310fc7aeab339315480f2249
David Coles <coles.david@gmail.com>
2021-12-21 16:26:39 -0800

    2021: Day 21 - Part 1 & 2

--- 2021/day21/src/main.rs
+++ 2021/day21/src/main.rs
@@ -1,44 +1,97 @@
 //! Advent of Code 2021: Day 21
 //! https://adventofcode.com/2021/day/21
 
-use std::fs;
-use std::io;
-use std::path::Path;
+use std::collections::HashMap;
+
+const PLAYER1: usize = 10;
+const PLAYER2: usize = 1;
 
 fn main() {
-    let input = Input::from_file("day21/input.txt").expect("failed to read input");
-    println!("{:?}", input);
-
     // Part 1
-    println!("Part 1: {}", part1(&input));
+    println!("Part 1: {}", part1(PLAYER1, PLAYER2));
 
     // Part 2
-    println!("Part 2: {}", part2(&input));
+    println!("Part 2: {}", part2(PLAYER1, PLAYER2));
 }
 
-fn part1(input: &Input) -> usize {
-    0
-}
+fn part1(p1: usize, p2: usize) -> usize {
+    let mut die = PracticeDie::new(100);
 
-fn part2(input: &Input) -> usize {
-    0
-}
+    let mut player = 0;
+    let mut pos = [p1, p2];
+    let mut score = [0; 2];
+    loop {
+        let n = die.roll() + die.roll() + die.roll();
+        pos[player] = (pos[player] + n - 1) % 10 + 1;
+        score[player] += pos[player];
 
-#[derive(Debug, Clone)]
-struct Input {
-    values: Vec<String>,
-}
-
-impl Input {
-    fn from_file(path: impl AsRef<Path>) -> io::Result<Self> {
-        let input = fs::read_to_string(path)?;
-
-        let mut values = Vec::new();
-        for line in input.lines() {
-            values.push(line.to_string());
+        if score[player] >= 1000 {
+            // Return the score of the losing player multiplied by dice rolls
+            return score[(player + 1) % 2] * die.n_rolls;
         }
 
-        Ok(Input { values })
+        player = (player + 1) % 2;
+    }
+}
+
+fn part2(p1: usize, p2: usize) -> usize {
+    // Pre-calculate possible dice results
+    let mut rolls: HashMap<usize, usize> = HashMap::new();
+    for n in 0..3_usize.pow(3) {
+        let x = (n % 3) + 1;
+        let y = (n / 3 % 3) + 1;
+        let z = (n / 3 / 3 % 3) + 1;
+
+        *rolls.entry(x + y + z).or_default() += 1;
+    }
+
+    let mut board = HashMap::new();
+    board.insert([(p1, 0), (p2, 0)], 1);
+
+    let mut wins = [0; 2];
+    let mut player = 0;
+    loop {
+        let mut new_board = HashMap::new();
+        for (&state, &n_universes) in board.iter() {
+            let (pos, score) = state[player];
+            for (&roll, &n_copies) in rolls.iter() {
+                let new_pos = (pos + roll - 1) % 10 + 1;
+                let mut new_state = state.clone();
+                new_state[player] = (new_pos, score + new_pos);
+                *new_board.entry(new_state).or_default() += n_universes * n_copies;
+            }
+        }
+
+        let winning: Vec<_> = new_board.keys().copied().filter(|&state | state[player].1 >= 21).collect();
+        for win in winning {
+            // Stop tracking this state
+            wins[player] += new_board.remove(&win).unwrap();
+            if new_board.is_empty() {
+                // Return the greater number of wins
+                return wins.into_iter().max().unwrap()
+            }
+        }
+
+        board = new_board;
+        player = (player + 1) % 2;
+    }
+}
+
+struct PracticeDie {
+    sides: usize,
+    n_rolls: usize,
+}
+
+impl PracticeDie {
+    fn new(sides: usize) -> Self {
+        PracticeDie { sides, n_rolls: 0 }
+    }
+
+    fn roll(&mut self) -> usize {
+        let n = (self.n_rolls % self.sides) + 1;
+        self.n_rolls += 1;
+
+        n
     }
 }
 
@@ -48,11 +101,11 @@ mod test {
 
     #[test]
     fn test_part1() {
-        todo!();
+        assert_eq!(part1(4, 8), 739785);
     }
 
     #[test]
     fn test_part2() {
-        todo!();
+        assert_eq!(part2(4, 8), 444356092776315);
     }
 }
