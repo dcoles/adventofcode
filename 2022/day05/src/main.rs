825c7fc808d3462c0d71311bef2cdf4be7d58454
David Coles <coles.david@gmail.com>
2022-12-04 21:47:49 -0800

    2022: Solution for Day 5

--- 2022/day05/src/main.rs
+++ 2022/day05/src/main.rs
@@ -5,9 +5,10 @@ use std::fs;
 use std::io;
 use std::path::Path;
 
+use regex::Regex;
+
 fn main() {
     let input = Input::from_file("day05/input.txt").expect("failed to read input");
-    println!("{:?}", input);
 
     // Part 1
     println!("Part 1: {}", part1(&input));
@@ -16,30 +17,78 @@ fn main() {
     println!("Part 2: {}", part2(&input));
 }
 
-fn part1(_input: &Input) -> usize {
-    0
+fn part1(input: &Input) -> String {
+    let mut crates = input.crates.clone();
+
+    for step in &input.steps {
+        for _ in 0..step.count {
+            let c = crates[step.from - 1].pop().expect("tried to move missing crate");
+            crates[step.to - 1].push(c);
+        }
+    }
+
+    crates.iter().filter_map(|s| s.last()).collect()
 }
 
-fn part2(_input: &Input) -> usize {
-    0
+fn part2(input: &Input) -> String {
+    let mut crates = input.crates.clone();
+
+    for step in &input.steps {
+        let mut crane = Vec::new();
+
+        for _ in 0..step.count {
+            crane.push(crates[step.from - 1].pop().expect("tried to move missing crate"));
+        }
+
+        for _ in 0..step.count {
+            crates[step.to - 1].push(crane.pop().unwrap());
+        }
+    }
+
+    crates.iter().filter_map(|s| s.last()).collect()
 }
 
 #[derive(Debug, Clone)]
 struct Input {
-    values: Vec<String>,
+    crates: Vec<Vec<char>>,
+    steps: Vec<Move>,
 }
 
 impl Input {
     fn from_file(path: impl AsRef<Path>) -> io::Result<Self> {
         let input = fs::read_to_string(path)?;
 
-        let mut values = Vec::new();
-        for line in input.lines() {
-            values.push(line.to_string());
+        let (chunk1, chunk2) = input.split_once("\n\n").expect("failed to split chunks");
+
+        let mut crates = vec![vec![];9];
+        for line in chunk1.lines().rev().skip(1) {
+            let chars: Vec<char> = line.chars().collect();
+            for (i, c) in chars.into_iter().skip(1).step_by(4).enumerate() {
+                if c.is_alphabetic() {
+                    crates[i].push(c);
+                }
+            }
         }
 
-        Ok(Input { values })
+        let re = Regex::new(r"move (\d+) from (\d)+ to (\d)+").unwrap();
+
+        let mut steps = Vec::new();
+        for cap in re.captures_iter(chunk2) {
+            let count = cap[1].parse().unwrap();
+            let from = cap[2].parse().unwrap();
+            let to = cap[3].parse().unwrap();
+            steps.push(Move { count, from, to })
+        }
+
+        Ok(Input { crates, steps })
     }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Move {
+    count: usize,
+    from: usize,
+    to: usize,
 }
 
 #[cfg(test)]
@@ -48,11 +97,15 @@ mod test {
 
     #[test]
     fn test_part1() {
-        todo!();
+        let input = Input::from_file("example1.txt").unwrap();
+
+        assert_eq!(part1(&input), "CMZ")
     }
 
     #[test]
     fn test_part2() {
-        todo!();
+        let input = Input::from_file("example1.txt").unwrap();
+
+        assert_eq!(part2(&input), "MCD")
     }
 }
