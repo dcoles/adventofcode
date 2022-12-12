2a4568bcd092fb863b526168869179b6ddb17e4e
David Coles <coles.david@gmail.com>
2022-12-11 20:16:36 -0800

    2022: Prepare for Day 12

---
+++ 2022/day12/src/main.rs
@@ -0,0 +1,62 @@
+//! Advent of Code 2022: Day 12
+//! https://adventofcode.com/2022/day/12
+
+use std::fs;
+use std::io;
+use std::path::Path;
+
+fn main() {
+    let input = Input::from_file(format!("{}/input.txt", env!("CARGO_MANIFEST_DIR"))).expect("failed to read input");
+    println!("{:?}", input);
+
+    // Part 1
+    println!("Part 1: {}", part1(&input));
+
+    // Part 2
+    println!("Part 2: {}", part2(&input));
+}
+
+fn part1(input: &Input) -> usize {
+    0
+}
+
+fn part2(input: &Input) -> usize {
+    0
+}
+
+#[derive(Debug, Clone)]
+struct Input {
+    values: Vec<String>,
+}
+
+impl Input {
+    fn from_file(path: impl AsRef<Path>) -> io::Result<Self> {
+        let input = fs::read_to_string(path)?;
+
+        let mut values = Vec::new();
+        for line in input.lines() {
+            values.push(line.to_string());
+        }
+
+        Ok(Input { values })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = Input::from_file("example1.txt").unwrap();
+
+        assert_eq!(part1(&input), 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = Input::from_file("example1.txt").unwrap();
+
+        assert_eq!(part2(&input), 0);
+    }
+}
