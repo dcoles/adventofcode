ca75fac7a47bd7206dbb6582c357635cada6d83e
David Coles <coles.david@gmail.com>
2022-12-13 01:47:31 -0800

    2022: Reimplement Day 13 using serde_json

--- 2022/day13/src/main.rs
+++ 2022/day13/src/main.rs
@@ -8,6 +8,9 @@ use std::io;
 use std::path::Path;
 use std::str::FromStr;
 
+use serde_json::Value;
+use serde_json::json;
+
 fn main() {
     let input = Input::from_file(format!("{}/input.txt", env!("CARGO_MANIFEST_DIR"))).expect("failed to read input");
 
@@ -42,8 +45,8 @@ fn part1(input: &Input) -> usize {
 fn part2(input: &Input) -> usize {
     let mut packets = input.values.clone();
 
-    let divider2: Item = "[[2]]".parse().unwrap();
-    let divider6: Item = "[[6]]".parse().unwrap();
+    let divider2: Item = Item(json!([[2]]));
+    let divider6: Item = Item(json!([[6]]));
 
     // Divider packets
     packets.push(divider2.clone());
@@ -58,129 +61,90 @@ fn part2(input: &Input) -> usize {
         .product()
 }
 
-/// An Item (either a List or a Value).
-#[derive(Clone, PartialEq, Eq, Ord)]
-enum Item {
-    List(Vec<Item>),
-    Value(u64),
-}
-
-impl Item {
-    fn push(&mut self, item: Item) -> &mut Item {
-        match self {
-            Item::List(xs) => {
-                xs.push(item);
-
-                xs.last_mut().unwrap()
-            }
-            _ => panic!("Tried to push to a value"),
-        }
-    }
-}
+/// A thin wrapper around `Value`.
+#[derive(Clone, PartialEq, Eq)]
+struct Item(Value);
 
 impl PartialOrd for Item {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        match (self, other) {
-            (Item::List(ls), Item::List(rs)) => {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Item {
+    fn cmp(&self, other: &Self) -> Ordering {
+        value_cmp(&self.0, &other.0)
+    }
+}
+
+impl Debug for Item {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Item({})", serde_json::to_string(&self.0).unwrap())
+    }
+}
+
+impl FromStr for Item {
+    type Err = serde_json::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        serde_json::from_str(s).map(Item)
+    }
+}
+
+fn value_cmp(left: &Value, right: &Value) -> Ordering {
+        match (left, right) {
+            (Value::Array(ls), Value::Array(rs)) => {
                 for i in 0.. {
                     if i == ls.len() && i == rs.len() {
                         // Ran out of items in both lists
-                        return Some(Ordering::Equal);
+                        return Ordering::Equal;
                     } else if i == ls.len() {
                         // Left list runs out of items
-                        return Some(Ordering::Less);
+                        return Ordering::Less;
                     } else if i == rs.len() {
                         // Right list runs out of items
-                        return Some(Ordering::Greater)
+                        return Ordering::Greater;
                     } else {
                         // Check each item
-                        let ord = ls[i].partial_cmp(&rs[i]);
+                        let ord = Item(ls[i].clone()).cmp(&Item(rs[i].clone()));
 
-                        if !matches!(ord, Some(Ordering::Equal)) {
-                            return ord;
+                        if !matches!(ord, Ordering::Equal) {
+                            return ord.clone();
                         }
                     }
                 }
 
                 unreachable!();
             },
-            (Item::List(_), Item::Value(r)) => {
+            (Value::Array(_), Value::Number(r)) => {
                 // Convert integer to a list
-                let rs = Item::List(vec![Item::Value(*r)]);
+                let rs = json!([r]);
 
-                self.partial_cmp(&rs)
+                value_cmp(left, &rs)
             },
-            (Item::Value(l), Item::List(_)) => {
+            (Value::Number(l), Value::Array(_)) => {
                 // Convert integer to a list
-                let ls = Item::List(vec![Item::Value(*l)]);
+                let ls = json!([l]);
 
-                ls.partial_cmp(other)
+                value_cmp(&ls, right)
             },
-            (Item::Value(l), Item::Value(r)) => {
+            (Value::Number(l), Value::Number(r)) => {
+                let l = l.as_u64().unwrap();
+                let r = r.as_u64().unwrap();
+
                 if l < r {
                     // Left integer lower than right
-                    Some(Ordering::Less)
+                    Ordering::Less
                 } else if l > r {
                     // Left integer higher than right
-                    Some(Ordering::Greater)
+                    Ordering::Greater
                 } else {
                     // Check next part
-                    Some(Ordering::Equal)
+                    Ordering::Equal
                 }
             },
+            _ => panic!("Can't compare {:?} and {:?}", left, right)
         }
-    }
-}
-
-impl FromStr for Item {
-    type Err = io::Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut stack: Vec<Item> = vec![];
-
-        let mut value = None;
-        for c in s.chars() {
-            if value.is_some() && !c.is_ascii_digit() {
-                stack.last_mut().unwrap().push(Item::Value(value.unwrap()));
-                value = None;
-            }
-
-            match c {
-                '[' => {
-                    stack.push(Item::List(vec![]));
-                },
-                ']' => {
-                    let item = stack.pop().unwrap();
-
-                    if let Some(last) = stack.last_mut() {
-                        last.push(item);
-                    } else {
-                        return Ok(item);
-                    }
-                },
-                c if c.is_ascii_digit() => {
-                    let d = c.to_digit(10).unwrap().into();
-                    if let Some(value) = &mut value {
-                        *value = *value * 10 + d;
-                    } else {
-                        value = Some(d);
-                    }
-                },
-                _ => (),
-            }
-        }
-
-        Err(io::Error::new(io::ErrorKind::Other, "Truncated input"))
-    }
-}
-
-impl Debug for Item {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Item::List(xs) => write!(f, "{:?}", xs),
-            Item::Value(x) => write!(f, "{}", x),
-        }
-    }
 }
 
 #[derive(Debug, Clone)]
@@ -194,7 +158,7 @@ impl Input {
 
         let mut values = Vec::new();
         for line in input.replace("\n\n", "\n").lines() {
-            values.push(line.parse::<Item>().unwrap());
+            values.push(line.parse().unwrap());
         }
 
         Ok(Input { values })
