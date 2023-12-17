c01fbfe696418f6a939d51dc42102517f93b2961
David Coles <dcoles@dcoles.net>
2023-12-17 14:30:02 -0800

    2023: Cleanup Day 17

--- 2023/day17/src/main.rs
+++ 2023/day17/src/main.rs
@@ -5,12 +5,8 @@ use std::collections::{BTreeMap, HashMap};
 use std::{fs, io};
 use std::path::Path;
 
-const MAX_CONSEC: usize = 3;
-
 fn main() {
     let input = Input::from_file(format!("{}/input.txt", env!("CARGO_MANIFEST_DIR"))).expect("failed to read input");
-    //let input = Input::from_file(format!("{}/example1.txt", env!("CARGO_MANIFEST_DIR"))).expect("failed to read input");
-    //let input = Input::from_file(format!("{}/example2.txt", env!("CARGO_MANIFEST_DIR"))).expect("failed to read input");
 
     // Part 1
     println!("Part 1: {}", part1(&input));
@@ -20,96 +16,52 @@ fn main() {
 }
 
 fn part1(input: &Input) -> u32 {
-    let start = Pos(0, 0);
-    let end = Pos(input.width - 1, input.height - 1);
-
-    let mut edge: Vec<(u32, Entry)> = [(0, Entry { pos: start, direction: Direction::Down, consec: 0}), (0, Entry { pos: start, direction: Direction::Right, consec: 0})].into_iter().collect();
-    let mut best_so_far: HashMap<Entry, (u32, usize)> = edge.iter().map(|(_, entry)| (*entry, (0, 0))).collect();
-
-    while let Some((_, cur)) = edge.pop() {
-        let (heat_loss, _) = best_so_far[&cur];
-        if cur.pos == end {
-            return heat_loss;
-        }
-
-        for dir in [cur.direction, cur.direction.left(), cur.direction.right()] {
-            let adj = cur.pos.to(dir);
-            if !input.valid(adj) {
-                continue;
-            }
-
-            let new_consec = if dir == cur.direction { cur.consec + 1 } else { 1 };
-            if new_consec > MAX_CONSEC {
-                continue;
-            }
-
-            let new = Entry { pos: adj, direction: dir, consec: new_consec };
-            let new_loss = heat_loss + input.map[&adj];
-
-            if let Some(&best) = best_so_far.get(&new) {
-                if new_loss > best.0 || new_loss == best.0 && new_consec >= best.1 {
-                    continue;
-                }
-            }
-
-            let estimate = ((end.0 - adj.0) + (end.1 - adj.1)) as u32;
-
-            best_so_far.insert(new, (new_loss, new_consec));
-            edge.push((new_loss + estimate, new));
-        }
-
-        // Sort so lowest loss is at the end (where we will next pop)
-        edge.sort_by_key(|(est, _)| u32::MAX - *est);
-    }
-
-    panic!("No solution found!")
+    simulate(input, 3, 0)
 }
 
 fn part2(input: &Input) -> u32 {
+    simulate(input, 10, 4)
+}
+
+fn simulate(input: &Input, max_consec: i32, min_consec: i32) -> u32 {
     let start = Pos(0, 0);
     let end = Pos(input.width - 1, input.height - 1);
 
-    let mut edge: Vec<(u32, Entry)> = [(0, Entry { pos: start, direction: Direction::Right, consec: 0}), (0, Entry { pos: start, direction: Direction::Down, consec: 0})].into_iter().collect();
+    let mut edge: Vec<(u32, Entry)> = [(0, Entry { pos: start, direction: Direction::Right }), (0, Entry { pos: start, direction: Direction::Down })].into_iter().collect();
     let mut best_so_far: HashMap<Entry, u32> = edge.iter().map(|(_, entry)| (*entry, 0)).collect();
 
     while let Some((_, cur)) = edge.pop() {
-        let heat_loss = best_so_far[&cur];
+        let cur_heat_loss = best_so_far[&cur];
         if cur.pos == end {
-            return heat_loss;
+            return cur_heat_loss;
         }
 
         for dir in [cur.direction.left(), cur.direction.right()] {
-            let mut new_loss = heat_loss;
-            for n_blocks in 1..=10 {
+            let mut heat_loss = cur_heat_loss;
 
-                let adj = match dir {
-                    Direction::Up => Pos(cur.pos.0, cur.pos.1 - n_blocks),
-                    Direction::Down => Pos(cur.pos.0, cur.pos.1 + n_blocks),
-                    Direction::Left => Pos(cur.pos.0 - n_blocks, cur.pos.1),
-                    Direction::Right => Pos(cur.pos.0 + n_blocks, cur.pos.1),
-                };
+            for n_blocks in 1..=max_consec {
+                let adj = cur.pos.to(dir, n_blocks);
 
-                new_loss += match input.map.get(&adj) {
+                heat_loss += match input.map.get(&adj) {
                     None => continue,
                     Some(n) => *n,
                 };
 
-
-                if n_blocks < 4 {
+                if n_blocks < min_consec {
                     // Can't stop or turn yet
                     continue;
                 }
 
-                let new = Entry { pos: adj, direction: dir, consec: 0 };
+                let new = Entry { pos: adj, direction: dir };
 
-                if best_so_far.contains_key(&new) && best_so_far[&new] <= new_loss {
+                if best_so_far.contains_key(&new) && best_so_far[&new] <= heat_loss {
                     continue;
                 }
 
                 let estimate = ((end.0 - adj.0) + (end.1 - adj.1)) as u32;
 
-                best_so_far.insert(new, new_loss);
-                edge.push((new_loss + estimate, new));
+                best_so_far.insert(new, heat_loss);
+                edge.push((heat_loss + estimate, new));
             }
         }
 
@@ -124,19 +76,18 @@ fn part2(input: &Input) -> u32 {
 struct Entry {
     pos: Pos,
     direction: Direction,
-    consec: usize,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Pos(i32, i32);
 
 impl Pos {
-    fn to(self, direction: Direction) -> Self {
+    fn to(self, direction: Direction, distance: i32) -> Self {
         match direction {
-            Direction::Up => Pos(self.0, self.1 - 1),
-            Direction::Down => Pos(self.0, self.1 + 1),
-            Direction::Left => Pos(self.0 - 1, self.1),
-            Direction::Right => Pos(self.0 + 1, self.1),
+            Direction::Up => Pos(self.0, self.1 - distance),
+            Direction::Down => Pos(self.0, self.1 + distance),
+            Direction::Left => Pos(self.0 - distance, self.1),
+            Direction::Right => Pos(self.0 + distance, self.1),
         }
     }
 }
@@ -177,10 +128,6 @@ struct Input {
 }
 
 impl Input {
-    fn valid(&self, pos: Pos) -> bool {
-        (0..self.width).contains(&pos.0) && (0..self.height).contains(&pos.1)
-    }
-
     fn from_file(path: impl AsRef<Path>) -> io::Result<Self> {
         let input = fs::read_to_string(path)?;
 
