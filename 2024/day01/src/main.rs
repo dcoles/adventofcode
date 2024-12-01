e20a69c96a8451c80355884222fb3573a7495f68
David Coles <david.coles@sony.com>
2024-11-30 21:31:54 -0800

    Solution to 2024 Day 1

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -2,6 +2,7 @@
 //! https://adventofcode.com/2024/day/1
 
 use std::{fs, io};
+use std::collections::HashMap;
 use std::path::Path;
 
 fn main() {
@@ -17,22 +18,61 @@ fn main() {
 }
 
 fn part1(input: &Input) -> usize {
-    0
+    let mut left = vec![];
+    let mut right = vec![];
+
+    for (l, r) in input.values.iter().cloned() {
+        left.push(l);
+        right.push(r);
+    }
+
+    left.sort();
+    right.sort();
+
+    let mut total_distance = 0;
+    for (l, r) in left.into_iter().zip(right.into_iter()) {
+        // Calculate distance between pairs
+        total_distance += l.abs_diff(r);
+    }
+
+    total_distance
 }
 
 fn part2(input: &Input) -> usize {
-    0
+    let mut left = Vec::new();
+    let mut right: HashMap<_, usize> = HashMap::new();
+
+    for (l, r) in input.values.iter().cloned() {
+        left.push(l);
+        *right.entry(r).or_default() += 1;
+    }
+
+    let mut similarity_score = 0;
+    for l in left {
+        let count = right.get(&l).copied().unwrap_or_default();
+
+        // Calculate similarity score
+        similarity_score += l * count;
+    }
+
+    similarity_score
 }
 
 #[derive(Debug, Clone)]
 struct Input {
-    values: Vec<String>,
+    values: Vec<(usize, usize)>,
 }
 
 impl Input {
     fn from_file(path: impl AsRef<Path>) -> io::Result<Self> {
         let input = fs::read_to_string(path)?;
-        let values = input.lines().map(str::to_string).collect();
+        let values = input.lines().map(|s| {
+            let mut split = s.split_ascii_whitespace();
+            let a = split.next().unwrap().parse().unwrap();
+            let b = split.next().unwrap().parse().unwrap();
+
+            (a, b)
+        }).collect();
 
         Ok(Self { values })
     }
@@ -46,27 +86,27 @@ mod test {
     fn test_part1() {
         let input = Input::from_file("example1.txt").unwrap();
 
-        assert_eq!(part1(&input), 0);
+        assert_eq!(part1(&input), 11);
     }
 
     #[test]
     fn test_part1_solution() {
         let input = Input::from_file("input.txt").unwrap();
 
-        assert_eq!(part1(&input), 0);
+        assert_eq!(part1(&input), 1722302);
     }
 
     #[test]
     fn test_part2() {
         let input = Input::from_file("example1.txt").unwrap();
 
-        assert_eq!(part2(&input), 0);
+        assert_eq!(part2(&input), 31);
     }
 
     #[test]
     fn test_part2_solution() {
         let input = Input::from_file("input.txt").unwrap();
 
-        assert_eq!(part2(&input), 0);
+        assert_eq!(part2(&input), 20373490);
     }
 }
